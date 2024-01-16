f66bbe83fff2b6f243423abce18228a699e927f5
Daniel Schall <daschall@microsoft.com>
2024-01-16 08:35:15 -0800

    Break on assert failures

--- test/pch.cpp
+++ test/pch.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "pch.h"
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::FLAGS_gtest_death_test_style = "threadsafe";
+	::testing::FLAGS_gtest_break_on_failure = true;
+	return RUN_ALL_TESTS();
+}
