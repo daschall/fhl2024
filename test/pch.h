22f645535bfda63bafa03ef78eed9a04b462cdd6
Daniel Schall <daschall@microsoft.com>
2024-01-16 13:20:23 -0800

    add includes

--- test/pch.h
+++ test/pch.h
@@ -6,7 +6,16 @@
 
 #include "gtest/gtest.h"
 
+// For simplicity, include all SE/QP headers here.
+//
+
 #include "storeng/btreesession.h"
 #include "storeng/btree.h"
 
-#include "queryprocessing/operators.h"
+#include "queryprocessing/aggregate.h"
+#include "queryprocessing/btreescanner.h"
+#include "queryprocessing/constscan.h"
+#include "queryprocessing/filter.h"
+#include "queryprocessing/join.h"
+#include "queryprocessing/project.h"
+#include "queryprocessing/sort.h"
